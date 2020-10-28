b43115e9d928c741d1cad852cdff5254c59e2654
liyang <xxbarca@163.com>
2020-10-28 08:16:11 +0800

    新增banner

--- src/model/banner.js
+++ src/model/banner.js
@@ -1,10 +1,15 @@
-import { get, put } from '../lin/plugin/axios'
+import { get, put, post } from '../lin/plugin/axios'
 class Banner {
     static async getBanners(page = 0, count = 10) {
         const res = await get('/v1/banner/page', {
             page,
             count
         })
+        return res
+    }
+    
+    static async addBanner(data) {
+        const res = await post('/v1/banner', data)
         return res
     }
     
--- src/view/banner/banner-create.vue
+++ src/view/banner/banner-create.vue
@@ -1,17 +1,106 @@
 <template>
-	<div id="banner-create">
-		<div class="title">banner 列表</div>
+	<div class="container">
+		<div class="title">创建Banner</div>
+		<div class="wrap">
+			<el-row>
+				<el-col :lg="16" :md="20" :sm="24" :xs="24">
+					<el-form :model="form" status-icon ref="form" label-width="100px" @submit.native.prevent>
+						<el-form-item label="名称" prop="name">
+							<el-input size="medium" v-model="form.name" placeholder="请填写名称"></el-input>
+						</el-form-item>
+						<el-form-item label="标题" prop="title">
+							<el-input size="medium" v-model="form.title" placeholder="请填写标题"></el-input>
+						</el-form-item>
+						<el-form-item label="主图" prop="img">
+							<upload-imgs ref="uploadEle" :max-num="maxNum" :value="initData" />
+						</el-form-item>
+						<el-form-item label="描述" prop="description">
+							<el-input size="medium" v-model="form.description" type="textarea" :rows="4" placeholder="请填写描述">
+							</el-input>
+						</el-form-item>
+						<el-form-item class="submit">
+							<el-button
+									type="primary"
+									v-permission="{ permission: '创建Banner', type: 'disabled' }"
+									@click="submitForm('form')"
+							>保 存</el-button
+							>
+							<el-button @click="resetForm('form')">重 置</el-button>
+						</el-form-item>
+					</el-form>
+				</el-col>
+			</el-row>
+		</div>
 	</div>
 </template>
 
 <script>
+    import UploadImgs from '@/component/base/upload-image'
+    import Banner from '../../model/banner'
     export default {
         name: 'banner-create',
+        components: { UploadImgs },
+        data() {
+            return {
+                form: {
+                    name: '',
+                    title: '',
+                    description: '',
+                    img: '',
+                },
+                initData: [],
+                maxNum: 1,
+            }
+        },
+		methods: {
+            async submitForm(formName) {
+                await this.getValue()
+                const form = { ...this.form }
+                const res = await Banner.addBanner(form)
+                if (res.code < window.MAX_SUCCESS_CODE) {
+                    this.$message.success(`${res.message}`)
+                    this.resetForm(formName)
+                    this.$confirm('是否跳转到Banner列表页？', '提示', {
+                        confirmButtonText: '是',
+                        cancelButtonText: '否',
+                        type: 'info',
+                    }).then(async () => {
+                        await this.$router.push({ path: '/banner/list' })
+                    })
+                }
+			},
+            async getValue() {
+                const val = await this.$refs.uploadEle.getValue()
+                if (val && val.length > 0) {
+                    this.form.img = val[0].display
+                }
+            },
+            // 重置表单
+            resetForm(formName) {
+                this.$refs[formName].resetFields()
+            },
+        }
     }
 </script>
 
 <style scoped lang="scss">
-	#banner-create {
+	.container {
+		.title {
+			height: 59px;
+			line-height: 59px;
+			color: $parent-title-color;
+			font-size: 16px;
+			font-weight: 500;
+			text-indent: 40px;
+			border-bottom: 1px solid #dae1ec;
+		}
 
+		.wrap {
+			padding: 20px;
+		}
+
+		.submit {
+			float: left;
+		}
 	}
 </style>
