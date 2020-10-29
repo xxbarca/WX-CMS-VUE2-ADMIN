76c12044f6df5f35bcc7e6fcc8f1b7493a665362
liyang <xxbarca@163.com>
2020-10-29 21:06:29 +0800

    删除主题

--- src/view/theme/theme-list.vue
+++ src/view/theme/theme-list.vue
@@ -110,7 +110,22 @@
                 this.showEdit = false
                 this.getThemes()
 			},
-            handleDelete() {},
+            handleDelete(val) {
+                this.$confirm('此操作将永久删除该项, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning',
+                }).then(async () => {
+                    const res = await Theme.deleteTheme(val.id)
+                    if (res.code < window.MAX_SUCCESS_CODE) {
+                        await this.getThemes()
+                        this.$message({
+                            type: 'success',
+                            message: `${res.message}`,
+                        })
+                    }
+                })
+			},
             async getThemes() {
                 const page = this.currentPage - 1
                 const count = this.pageCount
